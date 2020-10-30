f17f580892622e31c7542f96f32de23c91e36ef8
Jadelyn Libios <44410354+jaylibios@users.noreply.github.com>
2020-10-29 22:37:36 -0500

    Created site

---
+++ src/locations.js
@@ -0,0 +1,10 @@
+const areaLocations = [
+
+    { area: "northside", name: "Phobia", url: "http://www.darke.com/", address1: "5250 S Sam Houston Pkwy E. (BW-8)", address2: "Between 288 & I-45 S. Houston, TX 77048" },
+    { area: "southside", name: "Screamfest", url: "http://houstonhauntedhouses.com/", address1: "1500 Elton St.", address2: "Houston, TX 77034" },
+    { area: "southside", name: "Ghost Tours", url: "http://ghosttourstexas.com/", address1: "911 24th St.", address2: "Galveston, TX 77550" }
+
+
+];
+
+export {areaLocations};
---
+++ src/script.js
@@ -0,0 +1,26 @@
+import { areaLocations } from './locations.js';
+
+$(document).ready(function(){
+
+    const areas = ["Northside", "Southside", "Central", "Katy", "Westside", "Eastside"];
+    let navigation = document.getElementById("navigation");
+    let location = document.getElementById("loc");
+
+    // Populate navigation
+    areas.forEach(a => navigation.innerHTML += ` <li><a href="#locations" class='area' id='${a}'>${a}</a></li> `);
+
+    // Show location information on click
+    $(".area").click((event) => {
+        location.innerHTML = "";
+        let area = event.target.innerHTML.toLowerCase();
+        let list = areaLocations.filter(al => al.area == area);
+        
+        list.forEach(elem => {
+            location.innerHTML += ` 
+            <p class="name">${elem.name}</p>
+            <p class="address">${elem.address1}<br>${elem.address2}</p>
+            <p class="website"><a href="${elem.url}" target="_blank">Go to Website</a></p> `;
+        });
+    });
+
+});
